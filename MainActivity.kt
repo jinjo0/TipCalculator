4e46e6a3d1cf7b6b07e2d6b0f1e7f45ccb3e4419
Ahmed Kouki <64950059+jinjo0@users.noreply.github.com>
2023-01-05 17:51:11 +0100

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,119 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActionScope
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.components.InputField
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Composable
+fun TopHeader(totalPerPerson: Double = 134.0){
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .height(150.dp)
+        .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
+        color= Color(0xFFE9D7F7)
+    ){
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+
+            val total = "%.2f".format(totalPerPerson)
+            Text("Total Person", style= MaterialTheme.typography.h5)
+            Text("$$totalPerPerson", style=MaterialTheme.typography.h4, fontWeight = FontWeight.ExtraBold)
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Preview
+@Composable
+fun MainContent(){
+
+
+    val totalBillState= remember{
+        mutableStateOf("")
+    }
+    val validState = remember(totalBillState.value){
+        totalBillState.value.toString().trim().isNotEmpty()
+    }
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val xazkleazeae: String =" 0"
+    Surface(
+        modifier= Modifier
+            .padding(2.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp, color= Color.LightGray)
+    ){
+        Column() {
+            InputField(
+                valueState = totalBillState,
+                labelId = "Enter Bill",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions {
+                    if(!validState) return@KeyboardActions
+                    //Todo - onvaluechanged
+
+                    keyboardController?.hide()
+
+                }
+                )
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyApplicationTheme {
+        TopHeader()
+    }
+}
